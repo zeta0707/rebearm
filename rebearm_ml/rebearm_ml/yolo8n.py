382d00eb397ea03e0c79fd9a187aae47a9efb555
ZETA7 <zeta0707@gmail.com>
2024-12-15 17:43:03 +0900

    Update yolo8n.py
    Finally yolo8n.py is working

--- rebearm_ml/rebearm_ml/yolo8n.py
+++ rebearm_ml/rebearm_ml/yolo8n.py
@@ -43,18 +43,27 @@ class PersonDetectionNode(Node):
         self.bridge = CvBridge()
         self.image_sub = self.create_subscription(Image, "/image_raw",self.callback, qos_profile_sensor_data)
         print ("<< Subscribed to topic /image_raw")
+        self.image_pub = self.create_publisher(Image, '/image_yolo', 10)
+        print ("<< Publisher to topic /image_yolo")
 
         # Initialize YOLO model
         rosPath = os.path.expanduser('~/ros2_ws/src/rebearm/rebearm_ml/rebearm_ml/')
         yolov8n = rosPath + "yolov8n.pt"
         self.model = YOLO(yolov8n)  # Load the smallest YOLOv8 model
 
-    def callback(self,data):
-        # Convert images to numpy arrays
-        color_image = np.asanyarray(data)
-        print(color_image.shape)
+    def callback(self,msg):
+        try:
+            # Convert ROS image message to OpenCV image
+            cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+            # Convert to NumPy array
+            numpy_array = np.asanyarray(cv_image)
+            #print("Image shape:", numpy_array.shape)
+        
+        except Exception as e:
+            self.get_logger().error(f'Conversion error: {str(e)}')
+
         # Run YOLO detection
-        results = self.model(color_image)
+        results = self.model(numpy_array)
 
         # Process results
         for result in results:
@@ -65,13 +74,19 @@ class PersonDetectionNode(Node):
                     # Calculate the center of the bounding box
                     center_x = (x1 + x2) // 2
                     center_y = (y1 + y2) // 2
-
-                    cv2.rectangle(color_image, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                    print(center_x, center_y)
-
-        # Display the image
-        cv2.imshow('RealSense', data)
-        cv2.waitKey(1)
+                    print(f"center_x: {center_x}")
+                    print(f"center_y: {center_y}")
+                    cv2.rectangle(numpy_array, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                    try:
+                        # Convert NumPy array to ROS Image message
+                        ros_image = self.bridge.cv2_to_imgmsg(numpy_array, encoding="bgr8")
+                        
+                        # Publish the image
+                        self.image_pub.publish(ros_image)
+                        self.get_logger().info('Publishing image')
+                    
+                    except Exception as e:
+                        self.get_logger().error(f'Error publishing image: {str(e)}')
 
 
 def main(args=None):
