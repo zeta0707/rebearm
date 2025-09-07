c330d901bda5bdf8d338c225b76e99457b22b2be
ZETA7 <zeta0707@gmail.com>
2025-09-07 13:22:36 +0900

    telelo_joy.launch can override port
    Don't use teleop_joy.yaml, but can input parameter port

--- rebearm_teleop/launch/joy_all.launch.py
+++ rebearm_teleop/launch/joy_all.launch.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # Author: ChangWhan Lee
-import os
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
@@ -9,17 +8,12 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
   joy_dev = LaunchConfiguration('joy_dev')
-  teleop_joy_parameter = LaunchConfiguration(
-    'teleop_joy_parameter',
-    default=os.path.join(
-      get_package_share_directory('rebearm_teleop'),
-      'param/teleop_joy.yaml'
-    )
-  )
+  port = LaunchConfiguration('port')
+
   return LaunchDescription([
-    DeclareLaunchArgument('joy_config', default_value='xbox'),
     DeclareLaunchArgument('joy_dev', default_value='/dev/input/js0'),
-    DeclareLaunchArgument('teleop_joy_parameter', default_value=teleop_joy_parameter),
+    DeclareLaunchArgument('port', default_value='/dev/buslinker2'),
+    
     Node(
       package='joy', executable='joy_node', name='joy_node',
       parameters=[{
@@ -32,7 +26,10 @@ def generate_launch_description():
       package='rebearm_teleop', executable='teleop_joy', name='teleop_joy_node',
       output='screen',
       emulate_tty=True,
-      parameters=[teleop_joy_parameter],
+      parameters=[{
+        'port': port,
+        'max_deg': 120,
+        'step_deg': 5,
+      }],
     ),
-
   ])
